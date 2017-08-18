52e42127c218734655bf79d83ae5685c31f4c05a
Infantsabin <arockiamson@gmail.com>
2017-08-18 13:51:57 +0530

    Create fibo.py

---
+++ fibo.py
@@ -0,0 +1,19 @@
+nterms=10
+
+
+n1 = 0
+n2 = 1
+count = 0
+if nterms <= 0:
+   print("0")
+elif nterms == 1:
+  
+   print(n1)
+else:
+
+   while count < nterms:
+       print(n1)
+       nth = n1 + n2
+       n1 = n2
+       n2 = nth
+       count += 1
